f0046187ae17c7872b714f74e8428959657af1ea
Dimitrije Pavlov <dpavlov@college.harvard.edu>
2023-02-05 16:10:42 -0500

    Add and document error handling in chat262_protocol

--- chat262_protocol.cc
+++ chat262_protocol.cc
@@ -6,14 +6,46 @@
 
 namespace chat262 {
 
-message_header message_header::deserialize(
-    const std::vector<uint8_t>& hdr_data) {
-    message_header hdr;
-    memcpy(&hdr, hdr_data.data(), sizeof(message_header));
+const char* message_type_lookup(uint16_t msg_type) {
+    switch (msg_type) {
+        case msgtype_registration_request:
+            return "Registration request";
+        case msgtype_registration_response:
+            return "Registration response";
+        case msgtype_login_request:
+            return "Login request";
+        case msgtype_login_response:
+            return "Login response";
+        case msgtype_logout_request:
+            return "Logout request";
+        case msgtype_logout_response:
+            return "Logout response";
+        default:
+            return "Unknown";
+    }
+}
+
+const char* status_code_lookup(uint32_t stat_code) {
+    switch (stat_code) {
+        case status_code_ok:
+            return "OK";
+        case status_code_invalid_credentials:
+            return "Invalid username and/or password";
+        default:
+            return "Unknown";
+    }
+}
+
+status message_header::deserialize(const std::vector<uint8_t>& data,
+                                   message_header& hdr) {
+    if (data.size() != sizeof(message_header)) {
+        return status::error;
+    }
+    memcpy(&hdr, data.data(), sizeof(message_header));
     hdr.version_ = le16toh(hdr.version_);
     hdr.type_ = le16toh(hdr.type_);
     hdr.body_len_ = le32toh(hdr.body_len_);
-    return hdr;
+    return status::ok;
 }
 
 std::shared_ptr<message> registration_request::serialize(
@@ -38,9 +70,13 @@ std::shared_ptr<message> registration_request::serialize(
     return msg;
 }
 
-void registration_request::deserialize(const std::vector<uint8_t>& data,
-                                       std::string& username,
-                                       std::string& password) {
+status registration_request::deserialize(const std::vector<uint8_t>& data,
+                                         std::string& username,
+                                         std::string& password) {
+    // Ensure we can at least read username and password length
+    if (data.size() < sizeof(registration_request)) {
+        return status::error;
+    }
     const uint8_t* msg_body = data.data();
 
     uint32_t user_len_le;
@@ -51,12 +87,18 @@ void registration_request::deserialize(const std::vector<uint8_t>& data,
     memcpy(&pass_len_le, msg_body + 4, sizeof(uint32_t));
     uint32_t pass_len = le32toh(pass_len_le);
 
+    // Cannot proceed if there is a mismatch of size
+    if (2 * sizeof(uint32_t) + user_len + pass_len != data.size()) {
+        return status::error;
+    }
+
     username.assign(msg_body + 8, msg_body + 8 + user_len);
     password.assign(msg_body + 8 + user_len,
                     msg_body + 8 + user_len + pass_len);
+    return status::ok;
 }
 
-std::shared_ptr<message> registration_response::serialize(uint32_t status) {
+std::shared_ptr<message> registration_response::serialize(uint32_t stat_code) {
     uint32_t body_len = sizeof(registration_response);
     size_t total_len = sizeof(message_header) + body_len;
     std::shared_ptr<message> msg(static_cast<message*>(malloc(total_len)),
@@ -64,18 +106,24 @@ std::shared_ptr<message> registration_response::serialize(uint32_t status) {
     msg->hdr_.version_ = htole16(version);
     msg->hdr_.type_ = htole16(msgtype_registration_response);
     msg->hdr_.body_len_ = htole32(body_len);
-    uint32_t status_le = htole32(status);
-    memcpy(msg->body_, &status_le, sizeof(uint32_t));
+    uint32_t stat_code_le = htole32(stat_code);
+    memcpy(msg->body_, &stat_code_le, sizeof(uint32_t));
     return msg;
 }
 
-void registration_response::deserialize(const std::vector<uint8_t>& data,
-                                        uint32_t& status) {
+status registration_response::deserialize(const std::vector<uint8_t>& data,
+                                          uint32_t& stat_code) {
+    // We know the size upfront
+    if (data.size() != sizeof(registration_response)) {
+        return status::error;
+    }
     const uint8_t* msg_body = data.data();
 
-    uint32_t status_le;
-    memcpy(&status_le, msg_body, sizeof(uint32_t));
-    status = le32toh(status_le);
+    uint32_t stat_code_le;
+    memcpy(&stat_code_le, msg_body, sizeof(uint32_t));
+    uint32_t stat_code_he = le32toh(stat_code_le);
+    stat_code = le32toh(stat_code_le);
+    return status::ok;
 }
 
 std::shared_ptr<message> login_request::serialize(const std::string& username,
@@ -99,9 +147,13 @@ std::shared_ptr<message> login_request::serialize(const std::string& username,
     return msg;
 }
 
-void login_request::deserialize(const std::vector<uint8_t>& data,
-                                std::string& username,
-                                std::string& password) {
+status login_request::deserialize(const std::vector<uint8_t>& data,
+                                  std::string& username,
+                                  std::string& password) {
+    // Ensure we can at least read username and password length
+    if (data.size() < sizeof(login_request)) {
+        return status::error;
+    }
     const uint8_t* msg_body = data.data();
 
     uint32_t user_len_le;
@@ -112,12 +164,18 @@ void login_request::deserialize(const std::vector<uint8_t>& data,
     memcpy(&pass_len_le, msg_body + 4, sizeof(uint32_t));
     uint32_t pass_len = le32toh(pass_len_le);
 
+    // Cannot proceed if there is a mismatch of size
+    if (2 * sizeof(uint32_t) + user_len + pass_len != data.size()) {
+        return status::error;
+    }
+
     username.assign(msg_body + 8, msg_body + 8 + user_len);
     password.assign(msg_body + 8 + user_len,
                     msg_body + 8 + user_len + pass_len);
+    return status::ok;
 }
 
-std::shared_ptr<message> login_response::serialize(uint32_t status) {
+std::shared_ptr<message> login_response::serialize(uint32_t stat_code) {
     uint32_t body_len = sizeof(login_response);
     size_t total_len = sizeof(message_header) + body_len;
     std::shared_ptr<message> msg(static_cast<message*>(malloc(total_len)),
@@ -125,18 +183,24 @@ std::shared_ptr<message> login_response::serialize(uint32_t status) {
     msg->hdr_.version_ = htole16(version);
     msg->hdr_.type_ = htole16(msgtype_login_response);
     msg->hdr_.body_len_ = htole32(body_len);
-    uint32_t status_le = htole32(status);
-    memcpy(msg->body_, &status_le, sizeof(uint32_t));
+    uint32_t stat_code_le = htole32(stat_code);
+    memcpy(msg->body_, &stat_code_le, sizeof(uint32_t));
     return msg;
 }
 
-void login_response::deserialize(const std::vector<uint8_t>& data,
-                                 uint32_t& status) {
+status login_response::deserialize(const std::vector<uint8_t>& data,
+                                   uint32_t& stat_code) {
+    // We know the size upfront
+    if (data.size() != sizeof(login_response)) {
+        return status::error;
+    }
     const uint8_t* msg_body = data.data();
 
-    uint32_t status_le;
-    memcpy(&status_le, msg_body, sizeof(uint32_t));
-    status = le32toh(status_le);
+    uint32_t stat_code_le;
+    memcpy(&stat_code_le, msg_body, sizeof(uint32_t));
+    uint32_t stat_code_he = le32toh(stat_code_le);
+    stat_code = le32toh(stat_code_le);
+    return status::ok;
 }
 
 }  // namespace chat262
--- chat262_protocol.h
+++ chat262_protocol.h
@@ -1,6 +1,8 @@
 #ifndef _CHAT262_PROTOCOL_
 #define _CHAT262_PROTOCOL_
 
+#include "common.h"
+
 #include <cstddef>
 #include <cstdint>
 #include <memory>
@@ -12,23 +14,44 @@ namespace chat262 {
 static constexpr uint16_t version = 0x1;
 static constexpr uint16_t port = 45171;
 
+// Message types. Types in the range [101, 199] are client requestes. Types in
+// the range [201, 299] are server responses.
 enum message_type : uint16_t {
+    // Client requests
     msgtype_registration_request = 101,
-    msgtype_registration_response = 201,
     msgtype_login_request = 102,
-    msgtype_login_response = 202,
     msgtype_logout_request = 103,
-    msgtype_logout_response = 203
+
+    // Server responses
+    msgtype_registration_response = 201,
+    msgtype_login_response = 202,
+    msgtype_logout_response = 203,
 };
 
-enum status_code : uint32_t { status_ok = 0x0, status_invalid_user_pass = 0x1 };
+// Server response status codes
+enum status_code : uint32_t {
+    status_code_ok = 0x0,
+    status_code_invalid_credentials = 0x1,
+};
+
+// Look up the message type and returns a descriptive string
+const char* message_type_lookup(uint16_t msg_type);
+
+// Look up the server status code and returns a descriptive string
+const char* status_code_lookup(uint32_t stat_code);
 
 struct message_header {
     uint16_t version_;
     uint16_t type_;
     uint32_t body_len_;
 
-    static message_header deserialize(const std::vector<uint8_t>& hdr_data);
+    // Extract the header from `data` into `hdr`.
+    // @return ok    - success
+    // @return error - `data.size() != sizeof(message_header)`
+    //                 This is the fault of the local implementation, never of
+    //                 the remote party.
+    static status deserialize(const std::vector<uint8_t>& data,
+                              message_header& hdr);
 };
 
 struct message {
@@ -41,18 +64,45 @@ struct registration_request {
     uint32_t pass_len_;
     uint8_t user_pass_[];
 
+    // Form a complete registration request message from `username` and
+    // `password`.
     static std::shared_ptr<message> serialize(const std::string& username,
                                               const std::string& password);
-    static void deserialize(const std::vector<uint8_t>& data,
-                            std::string& username,
-                            std::string& password);
+
+    // Extract the username and password from `data`.
+    // `data` must contain the `registration_request` structure.
+    // @return ok    - success
+    // @return error - `data.size() != sizeof(registration_request) + user_len_
+    //                 + pass_len_`
+    //                 This is potentially the fault of the local
+    //                 implementation, if `data` was not resized to `body_len_`
+    //                 advertised in the message header. It could also be the
+    //                 fault of the remote party, if `body_len_` did not match
+    //                 what was provided in `user_len_` and `pass_len_`.
+    static status deserialize(const std::vector<uint8_t>& data,
+                              std::string& username,
+                              std::string& password);
 };
 
 struct registration_response {
-    uint32_t status;
+    uint32_t stat_code;
 
-    static std::shared_ptr<message> serialize(uint32_t status);
-    static void deserialize(const std::vector<uint8_t>& data, uint32_t& status);
+    // Form a complete registration response message from `stat_code`.
+    static std::shared_ptr<message> serialize(uint32_t stat_code);
+
+    // Extract the status code from `data`.
+    // `data` must contain the `registration_response` structure.
+    // @return ok    - success. There is no guarantee that `stat_code` is a
+    //                 valid member of the `status_code` enum, and local
+    //                 implementation should do further error-checking.
+    // @return error - `data.size() != sizeof(registration_response)`
+    //                 This is potentially the fault of the local
+    //                 implementation, if `data` was not resized to `body_len_`
+    //                 advertised in the message header. It could also be the
+    //                 fault of the remote party, if `body_len_` was incorrectly
+    //                 advertised in the message header.
+    static status deserialize(const std::vector<uint8_t>& data,
+                              uint32_t& stat_code);
 };
 
 struct login_request {
@@ -60,18 +110,44 @@ struct login_request {
     uint32_t pass_len_;
     uint8_t user_pass_[];
 
+    // Form a complete login request message from `username` and `password`.
     static std::shared_ptr<message> serialize(const std::string& username,
                                               const std::string& password);
-    static void deserialize(const std::vector<uint8_t>& data,
-                            std::string& username,
-                            std::string& password);
+
+    // Extract the username and password from `data`.
+    // `data` must contain the `login_request` structure.
+    // @return ok    - success
+    // @return error - `data.size() != sizeof(login_request) + user_len_
+    //                 + pass_len_`
+    //                 This is potentially the fault of the local
+    //                 implementation, if `data` was not resized to `body_len_`
+    //                 advertised in the message header. It could also be the
+    //                 fault of the remote party, if `body_len_` did not match
+    //                 what was provided in `user_len_` and `pass_len_`.
+    static status deserialize(const std::vector<uint8_t>& data,
+                              std::string& username,
+                              std::string& password);
 };
 
 struct login_response {
-    uint32_t status;
+    uint32_t stat_code;
 
-    static std::shared_ptr<message> serialize(uint32_t status);
-    static void deserialize(const std::vector<uint8_t>& data, uint32_t& status);
+    // Form a complete login response message from `stat_code`.
+    static std::shared_ptr<message> serialize(uint32_t stat_code);
+
+    // Extract the status code from `data`.
+    // `data` must contain the `login_response` structure.
+    // @return ok    - success. There is no guarantee that `stat_code` is a
+    //                 valid member of the `status_code` enum, and local
+    //                 implementation should do further error-checking.
+    // @return error - `data.size() != sizeof(login_response)`
+    //                 This is potentially the fault of the local
+    //                 implementation, if `data` was not resized to `body_len_`
+    //                 advertised in the message header. It could also be the
+    //                 fault of the remote party, if `body_len_` was incorrectly
+    //                 advertised in the message header.
+    static status deserialize(const std::vector<uint8_t>& data,
+                              uint32_t& stat_code);
 };
 
 struct logout_body {};
