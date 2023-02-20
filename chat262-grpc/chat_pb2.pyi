05e40f71c19f86eb69a63e71fed8aae37b37963c
Cynthia Chen <cynthia9chen@gmail.com>
2023-02-20 02:09:14 -0500

    add ip address option + rename folder

---
+++ chat262-grpc/chat_pb2.pyi
@@ -0,0 +1,73 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class ChatMessage(_message.Message):
+    __slots__ = ["body", "sender"]
+    BODY_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    body: str
+    sender: str
+    def __init__(self, sender: _Optional[str] = ..., body: _Optional[str] = ...) -> None: ...
+
+class Empty(_message.Message):
+    __slots__ = []
+    def __init__(self) -> None: ...
+
+class LoginRequest(_message.Message):
+    __slots__ = ["password", "username"]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    password: str
+    username: str
+    def __init__(self, username: _Optional[str] = ..., password: _Optional[str] = ...) -> None: ...
+
+class LoginResponse(_message.Message):
+    __slots__ = ["message", "success"]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    success: bool
+    def __init__(self, success: bool = ..., message: _Optional[str] = ...) -> None: ...
+
+class RegisterRequest(_message.Message):
+    __slots__ = ["password", "username"]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    password: str
+    username: str
+    def __init__(self, username: _Optional[str] = ..., password: _Optional[str] = ...) -> None: ...
+
+class RegisterResponse(_message.Message):
+    __slots__ = ["message", "success"]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    success: bool
+    def __init__(self, success: bool = ..., message: _Optional[str] = ...) -> None: ...
+
+class SendRequest(_message.Message):
+    __slots__ = ["body", "receiver", "sender"]
+    BODY_FIELD_NUMBER: _ClassVar[int]
+    RECEIVER_FIELD_NUMBER: _ClassVar[int]
+    SENDER_FIELD_NUMBER: _ClassVar[int]
+    body: str
+    receiver: str
+    sender: str
+    def __init__(self, sender: _Optional[str] = ..., receiver: _Optional[str] = ..., body: _Optional[str] = ...) -> None: ...
+
+class SendResponse(_message.Message):
+    __slots__ = ["message", "success"]
+    MESSAGE_FIELD_NUMBER: _ClassVar[int]
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    message: str
+    success: bool
+    def __init__(self, success: bool = ..., message: _Optional[str] = ...) -> None: ...
+
+class User(_message.Message):
+    __slots__ = ["username"]
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    def __init__(self, username: _Optional[str] = ...) -> None: ...
---
+++ chat262-grpc/client.py
@@ -0,0 +1,116 @@
+import grpc
+import chat_pb2
+import chat_pb2_grpc
+# import login
+import tkinter as tk
+import threading
+import sys
+ 
+# total arguments
+n_arg = len(sys.argv)
+# print(n_arg)
+# print("Total arguments passed:", n)
+ 
+# Arguments passed
+channel_name = ''
+if n_arg == 1:
+    channel_name = 'localhost:50051'
+elif n_arg == 2:
+    channel_name = sys.argv[0]
+
+# 10.250.143.105:50051
+channel = grpc.insecure_channel(channel_name)
+auth_stub = chat_pb2_grpc.AuthServiceStub(channel)
+chat_stub = chat_pb2_grpc.ChatServiceStub(channel)
+
+username = ''
+page_status = 0 # 0 = login/register prompt, 1 = home page
+
+def receive_messages():
+    global username
+    
+    while True:
+        message_list = chat_stub.ReceiveMessage(chat_pb2.User(username = username))
+        for m in message_list:
+            print(f'\n\n Received new message from {m.sender}: {m.body}\n\n>> Enter recipient username: ', end = '')
+            # print('Recipient username', end = '')
+
+def send_messages():
+    global username
+    while True:
+        receiver = input('\n>> Enter recipient username: ')
+        body = input('>> Enter message body: ')
+        request = chat_pb2.SendRequest(sender=username, receiver=receiver, body=body)
+        response = chat_stub.SendMessage(request)
+        
+        if not response.success:
+            print(response.message)
+
+def run_home():
+    global username
+    print('\n----------')
+    print("\nWELCOME TO THE CHAT HOME PAGE")
+
+    print('\nInbox [new messages since last login]:')
+    message_list = chat_stub.ReceiveMessage(chat_pb2.User(username = username))
+    empty_inbox = True
+    for m in message_list:
+        print(f'{m.sender}: {m.body}\n')
+        empty_inbox = False
+    if empty_inbox:
+        print("No new messages to show.")
+
+    user_list = chat_stub.GetUsers(chat_pb2.Empty())
+
+    print('\n----------')
+
+    print('\nChat with another user!')
+
+    users = []
+    for u in user_list:
+        users.append(u.username)
+    print('\nAll usernames: ', users)
+
+
+    threading.Thread(target = send_messages).start()
+    threading.Thread(target = receive_messages).start()
+    return 1
+
+
+def run_login():
+    global username
+    choice = input("\nRegister or Login?\n\n")
+
+    if 'r' in choice.lower():
+        print("Register with username and password.")
+        username = input(">> Username: ")
+        password = input(">> Password: ")
+        request = chat_pb2.RegisterRequest(username=username, password=password)
+        response = auth_stub.Register(request)
+        print(response.message)
+
+        if response.success:
+            return 1
+        else:
+            return 0
+
+
+    else:
+        print("Login with your username and password.")
+        username = input(">> Username: ")
+        password = input(">> Password: ")
+        request = chat_pb2.LoginRequest(username=username, password=password)
+        response = auth_stub.Login(request)
+        print(response.message)
+
+        if response.success:
+            return 1
+        else:
+            return 0
+
+
+
+while page_status == 0:
+    page_status = run_login()
+
+run_home()
